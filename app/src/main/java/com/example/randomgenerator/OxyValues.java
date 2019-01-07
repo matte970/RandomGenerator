6e5660451297f7f953c3311b63289a72ec08ba05
Mateo <be17b021@technikum-wien.at>
2019-01-07 21:03:28 +0100

    OxyValues class complete

--- app/src/main/java/com/example/randomgenerator/OxyValues.java
+++ app/src/main/java/com/example/randomgenerator/OxyValues.java
@@ -1,6 +1,31 @@
 package com.example.randomgenerator;
 
 public class OxyValues {
-    int value;
-    long time;
+    /**
+     * value of the data point (y-axis)
+     */
+    private int mvalue;
+    /**
+     * time of the data point (x-axis)
+     */
+    private long mtime;
+
+    /**
+     * Constructs a new {@link OxyValues} object
+     *
+     * @param value is value of the data point (y-axis)
+     * @param time  is time of the data point (x-axis)
+     */
+    public OxyValues(int value, long time) {
+        mvalue = value;
+        mtime = time;
+    }
+
+    public int getValue() {
+        return mvalue;
+    }
+
+    public long getTime() {
+        return mtime;
+    }
 }
