267b73934b6b507875ba5262826ede5a035c186f
Mateo <be17b021@technikum-wien.at>
2019-01-09 03:16:41 +0100

    finish

--- app/src/main/java/com/example/randomgenerator/RandomGenerator.java
+++ app/src/main/java/com/example/randomgenerator/RandomGenerator.java
@@ -1,5 +1,6 @@
 package com.example.randomgenerator;
 
+import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -42,38 +43,49 @@ public class RandomGenerator extends AppCompatActivity {
             int randomNumber;
             boolean b;
             randomNumber = r.nextInt(4) + 0;
-            b=r.nextBoolean();
+            b = r.nextBoolean();
 
-            if(b){
-                value+=randomNumber;
-            }else{
-                value-=randomNumber;
+            if (b) {
+                value += randomNumber;
+            } else {
+                value -= randomNumber;
             }
 
             if (!critical.isChecked()) {
-                if(value>100||value<90){
-                    value=95;
+                if (value > 100 || value < 90) {
+                    value = 95;
                 }
             } else {
-                if(value>90||value<60){
-                    value=89;
+                if (value > 90 || value < 60) {
+                    value = 89;
                 }
             }
 
             /**
              * Assigns value for x-axis using the app timestamp in Unix time
              * */
-
-            OxyValues OxyValues = new OxyValues(value, System.currentTimeMillis());
+            long time = System.currentTimeMillis();
+            OxyValues OxyValues = new OxyValues(value, time);
 
             /**
              * Displays a Toast whit the x and y values
              * */
 
-            Toast check = Toast.makeText(getApplicationContext(), "" + OxyValues.getValue() + "  " + OxyValues.getTime(), Toast.LENGTH_SHORT);
-            check.show();
-
+//            Toast check = Toast.makeText(getApplicationContext(), "" + OxyValues.getValue() + "  " + OxyValues.getTime(), Toast.LENGTH_SHORT);
+//            check.show();
+            /**
+             * Displays the Value on the screen
+             * */
             O2Values.setText("" + OxyValues.getValue());
+
+            /**
+             * Sends an Intent with the attributes of @link OxyValues as a broadcast
+             *  */
+            Intent intent = new Intent();
+            intent.setAction("PulsOXyMax");
+            intent.putExtra("Ox", OxyValues);
+            intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+            sendBroadcast(intent);
 
 
             /**
@@ -88,7 +100,7 @@ public class RandomGenerator extends AppCompatActivity {
     public void startGenerator(android.view.View next) {
 
         handler.post(runnableCode);
-        value=95;
+        value = 95;
 
 
     }
